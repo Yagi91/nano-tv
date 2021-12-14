26ae310f2dd7514fa1f91ab6a6526829bbc025e1
Yagi91 <bryantimah@gmail.com>
2021-12-14 01:06:37 +0100

    Update utils.js
    added comments  to utils file

--- src/utility/utils.js
+++ src/utility/utils.js
@@ -2,19 +2,19 @@ import { DateTime } from "luxon";
 import axios from "axios";
 
 const listing = (e, setList, list) => {
-  console.log("entered listx", list, setList, e);
-  const showId = e.currentTarget.getAttribute("movieid");
+  const showId = e.currentTarget.getAttribute("movieid"); //utilized the current target since the element is nested down in another component
   const showName = e.currentTarget.getAttribute("moviename");
-  const dateString = { ...DateTime.DATETIME_SHORT };
+  const dateString = { ...DateTime.DATETIME_SHORT }; //returns shorter date-time with slashes to easily fit in same line with the movie title
   let nextEp;
   let showExistIndex;
+  //ensure that clicking on the follow button twice removes that show from the list
   function showExist(show, index, array) {
     showExistIndex = index;
     return show.showName === showName;
   }
   if (list.length > 0 && list.some(showExist)) {
     setList((prevList) => {
-      let newList = prevList.slice();
+      let newList = prevList.slice(); //avoid mutating state in itself
       newList.splice(showExistIndex, 1);
       return newList;
     });
@@ -27,11 +27,11 @@ const listing = (e, setList, list) => {
       if (res.data._embedded) {
         console.log("data is embedded");
         nextEp = DateTime.fromISO(
+          //returns date object from the ISO string found as a Key-value of the nextExp in the Embedded property of the movie
           res.data["_embedded"].nextepisode.airstamp
         ).toLocaleString(dateString);
         setList((list) => {
-          let newList = list.slice();
-          console.log(newList);
+          let newList = list.slice(); //avoid mutating state in itself
           newList.push({
             showName: showName,
             nextEp: nextEp,
@@ -58,11 +58,13 @@ const listing = (e, setList, list) => {
     });
 };
 
+//submit a search query
 export const Submit = (e, setSearched, setUrl) => {
-  setSearched(true);
+  setSearched(true); //this will make the data API know that it is a search query and hence the embedded property of the movie is not return
   e.preventDefault();
   let query = document.getElementById("search-bar").value;
   if (query.trim().length > 0) {
+    //trim the empty spaces in from and behind the search term
     setUrl(`https://api.tvmaze.com/search/shows?q=${query}`);
   } else if (query.trim().length < 0) {
     return;
@@ -70,10 +72,11 @@ export const Submit = (e, setSearched, setUrl) => {
   document.getElementById("search-bar").value = "";
 };
 
+//displays a single page containing the full details of that particular movie
 export const displayPage = (e, setFocusShow, setFocusShowId, setShowPage) => {
   setFocusShow(e.currentTarget.getAttribute("moviename"));
   setFocusShowId(e.currentTarget.getAttribute("movieid"));
-  setShowPage(true);
+  setShowPage(true); //the page can be visible now
 };
 
 export default listing;
