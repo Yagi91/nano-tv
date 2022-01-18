0e52f9ae9deef32771502858ce81b582e03e918c
Yagi91 <bryantimah@gmail.com>
2022-01-18 08:00:34 +0100

    Update list.js

--- src/components/list.js
+++ src/components/list.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "../styles/list.css";
 import { handleList } from "../utility/utils";
 import { DateTime } from "luxon";
